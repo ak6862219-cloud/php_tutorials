4cac350e68131281027e11a6e325c96ec55dfd9e
adarsh kushwaha <ak6862219@gmail.com>
2026-03-17 10:08:21 +0000

    update

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+echo "Hello PhP <br>";
+echo "Name: Adarsh kushwaha<br>";
+echo "B.tech in computer engineering<br>";
+echo "Cgpa: 7.53<br>";
+echo "J.C.Bose university of Science and Technology, YMCA Faridabad<br>";
+echo "<a href='https://www.linkedin.com/in/adarsh-kushwaha-6ab371369/' target='_blank'>My LinkedIn Profile</a>";
+?>
